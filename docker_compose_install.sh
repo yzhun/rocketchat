c3abd9ddc3d8c50a4ff6e886a20761e803fa5c23
Yurii Zhun <yurii.zhun@gmail.com>
2019-05-14 12:54:50 +0200

    edit sh files

--- docker_compose_install.sh
+++ docker_compose_install.sh
@@ -1,3 +1,3 @@
 #sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
-sudo apt install -y docker docker-compose
+sudo apt install -y docker.io docker-compose
 sudo usermod -aG docker $(users)
